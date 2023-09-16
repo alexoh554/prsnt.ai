fb944fa66dc55d790f9f030d9365a67b26ac09b3
Alex Oh <alexoh0504@gmail.com>
2023-09-16 12:54:38 -0400

    Created Cohere backend summary generation:

--- backend/views.py
+++ backend/views.py
@@ -1,21 +1,51 @@
 from flask import Flask, jsonify
 from serpapi import GoogleSearch
+import cohere
 
-# SerpAPI endpoint 
-# Searches with query and returns first google image result
+SERP_API_KEY = "a240d146c529bc7e3c401e6ca82121c8b10f23be53d4b62537338bfbe7a11085"
+COHERE_API_KEY = "OkdezIHQB1oWjY7pziAjufhf3oHXsdiPgTnb8puW"
+
+
+# Presentation page
 def present():
+    # SerpAPI endpoint 
+    # Searches with query and returns first google image result
     image_query = "big joshua tree"
     
     params = {
     "q": image_query,
     "engine": "google_images",
     "ijn": "0",
-    "api_key": "a240d146c529bc7e3c401e6ca82121c8b10f23be53d4b62537338bfbe7a11085"
+    "api_key": SERP_API_KEY
     }    
     
     search = GoogleSearch(params)
     results = search.get_dict()
     images_results = results["images_results"]
     
-    return images_results[0]['original']
+    
+    # Cohere 
+    co = cohere.Client(COHERE_API_KEY)
+
+    # Get the text
+    text = "The blue whale (Balaenoptera musculus) is a marine mammal and a baleen whale. Reaching a maximum confirmed length of 29.9 meters (98 ft) and weighing up to 199 tonnes (196 long tons; 219 short tons), it is the largest animal known ever to have existed.[a]"
+    
+    # Use Cohere to summarize the text
+    response = co.summarize(
+        text = text,
+        length = 'medium',
+        format = 'bullets',
+        model = 'summarize-xlarge',
+        additional_command = '',
+        temperature = 0.4,
+        )
+
+    # Get the summary from the response
+    print(response)
+
+    return jsonify({
+        "image": images_results[0]['original'],
+        "summary": response[1]
+    })
+
     
